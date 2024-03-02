968f0d2d05940a45d3426591ff0a7849cb7cfdb4
EnricoCalvanese <142629535+EnricoCalvanese@users.noreply.github.com>
2024-03-02 00:48:56 -0800

    Create IMB2Colabfold.sh
    Slurm bash script that instructs the scheduler to run runcolabfold.py command with the necessary resources. Last command to run

---
+++ IMB2Colabfold.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Job name:
+#SBATCH --job-name=colabfold
+#SBATCH --account=fc_rnaseq
+#SBATCH --partition=savio3_gpu
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --qos=savio_lowprio
+#SBATCH --gres=gpu:A40:1
+#SBATCH --time=72:00:00
+##Command(s) to run:
+python runcolabfoldsavio3.py
